c7a96992ecb431a2216f05a4f9cf15c9321f4782
LuminarH <hyeyoung9115@gmail.com>
2024-02-27 19:54:15 +0900

    👔 Add: test

---
+++ test.js
@@ -0,0 +1,11 @@
+let url = new URL(
+  `http://www.aladin.co.kr/ttb/api/ItemOffStoreList.aspx?ttbkey=[]`
+);
+
+const getApi = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log("data", data);
+};
+
+getApi();
